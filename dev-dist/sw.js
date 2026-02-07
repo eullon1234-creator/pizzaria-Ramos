e75fe2a1ec61d6f76cf5c8fef5e757a4d061b5f0
Eullon Oliveira <seu-email-do-github@exemplo.com>
2026-02-07 15:02:39 -0300

    fix: atualizar a revisão do arquivo index.html no service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.s34cquf16cg"
+    "revision": "0.gs2ppfus8ec"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
